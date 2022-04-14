067d35fbaab4b160cd14ec5e5a6fe3e40bd6dafd
tomioke <40717020+tomioke@users.noreply.github.com>
2022-04-14 22:21:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-echo "Starting Node Latest Data... \n";
+DATA_DIR="./blockchain"
+CONFIG_DIR="./blockchain/config"
+EOS_PUB_KEY= #REPLACE THIS WITH ITS CORRESPONDING VALUE
+EOS_PRIV_KEY= #REPLACE THIS WITH ITS CORRESPONDING VALUE
+echo "Running Node Again... \n";
 set -ex;
 ulimit -n 65535
 ulimit -s 64000
 
-DATA_DIR="blockchain";
-
-SIGNING_PUB_KEY="VEX7Lmu76HHMYeKrA7wQ5JWsT2RJktHo6KDbNE27PTLUo1qXWz3pv" # Insert your public key here
-SIGNING_PRIV_KEY="5JW2qXzQB5xpd36k9EhKSVJDdf8k53hq26cJ6EiZuN6LjZQ1LMi" # Insert your private key here
-
-if [ ! -d $DATA_DIR ]; then
-  mkdir -p $DATA_DIR;
+if [ ! -d $CONFIG_DIR ]; then
+  mkdir -p $CONFIG_DIR;
+  cp ./config.ini $CONFIG_DIR/config.ini
 fi
 
 start_nodeos=$"nodeos \
-  --config-dir $DATA_DIR/config \
+  --config-dir $CONFIG_DIR \
   --data-dir $DATA_DIR/data \
   --blocks-dir $DATA_DIR/blocks \
   --disable-replay-opts \
